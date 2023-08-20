983d88397074314ad77bff9603126c7f3508dedc
Marcelo Meloni Bruneri <marcelombruneri@gmail.com>
2023-08-20 00:52:25 -0300

    versao ok gerando excel e iniciando tela de dialogo

--- query_result.py
+++ query_result.py
@@ -1,4 +1,14 @@
 import datetime
+from xlsxwriter import Workbook
+import tkinter as tk
+from tkinter import filedialog
+
+def abrir_arquivo():
+    arquivo = filedialog.askopenfilename(title="Selecione um arquivo", filetypes=[("Arquivos de Texto", "*.txt"), ("Todos os Arquivos", "*.*")])
+    if arquivo:
+        print(f"Arquivo selecionado: {arquivo}")
+    return arquivo
+
 
 def query_to_excel():
 
@@ -8,23 +18,27 @@ def query_to_excel():
     #                 +-----------------------------------------------------------
     # (linha)       1_³ 1234567890123456789 ³ 1234567890123456789 ³ A            ³       
     #                 +-----------------------------------------------------------
-    arq_query_result = open('D:\\SC\\query_resut.txt', 'r')
+
+    # Crie uma janela principal
+    root = tk.Tk()
+
+    # Crie um botão que, quando clicado, abre a caixa de diálogo de abertura de arquivo
+    botao = tk.Button(root, text="Abrir Arquivo", command=abrir_arquivo)
+    botao.pack()
+
+    # Inicie o loop principal da interface gráfica
+    # root.mainloop()
+
+    # arq_query_result = open('D:\\SC\\query_resut.txt', 'r')
+    arq_query_result = open(abrir_arquivo(), 'r')
 
     query_result = arq_query_result.readlines()
 
     # despreza primeira coluna do header '#'
     header = Util.procura_header(query_result)[1:]
-    csv_result = []
-    csv_result.append(';'.join(header) + '\n')
-
     dados = Util.procura_linhas(query_result)
-    for chave in dados:
-        csv_result.append(';'.join(dados[chave]) + '\n')
 
-    datahora = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
-    nome_arquivo = 'arq_result{}.txt'.format(datahora)
-    arq_result_csv = open(nome_arquivo, 'w')
-    arq_result_csv.writelines(csv_result)
+    Util.gerar_excel(header, dados)
 
 
 class Util:
@@ -77,12 +91,64 @@ class Util:
             if Util.ARG_LINHA in linha:
                 colunas = Util.quebra_linha(linha)
                 # despreza ultimo caracter que seria '_'
-                num_linha = colunas[0][:-1]
+                num_linha = int(colunas[0][:-1])
                 if num_linha not in linhas:
                     linhas[num_linha] = colunas[1:]
                 else:
                     linhas[num_linha] = linhas[num_linha] + colunas[1:]
         return linhas                
+
+    @classmethod
+    def definir_tamanho_planilha(cls, dados):
+        linhas = len(dados)
+        colunas = len(dados[1]) - 1
+        return (linhas, colunas)
+    
+    @classmethod
+    def gerar_csv(cls, header, dados):
+        csv_result = []
+        csv_result.append(';'.join(header) + '\n')
+        for chave in dados:
+            csv_result.append(';'.join(dados[chave]) + '\n')
+
+        datahora = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
+        nome_arquivo = 'arq_result{}.txt'.format(datahora)
+        arq_result_csv = open(nome_arquivo, 'w')
+        arq_result_csv.writelines(csv_result)
+
+    @classmethod
+    def gerar_excel(cls, header, dados):
+        tamanho_planilha = Util.definir_tamanho_planilha(dados)
+
+        # criar arquivo excel
+        workbook = Workbook('query_result.xlsx')
+        planilha = workbook.add_worksheet('QueryResult')
+
+        # definir tamanho das colunas (col_ini, col_fim, tam)
+        # planilha.set_column(0, tamanho_planilha[1], 15)
+        
+        # estilo_texto = workbook.add_format({'num_format': '@'})
+
+        planilha.add_table(0, 0, tamanho_planilha[0], tamanho_planilha[1], 
+                           {'data': list(dados.values()),
+                            'columns': Util.header_para_xlsxwriter(header)
+                            })
+        
+        planilha.autofit()
+
+        # planilha.write(1, 1, header[0], estilo_texto)
+
+        workbook.close()
+    
+    @classmethod
+    def header_para_xlsxwriter(cls, header):
+        # header_xlsx = []
+        # for nome_coluna in header:
+        #     print(nome_coluna)
+        #     header_xlsx.append({'header': nome_coluna})
+        # return header_xlsx
+        return [{'header': nome_coluna} for nome_coluna in header]
+        
 
 
 if __name__ == '__main__':
